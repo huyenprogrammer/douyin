8716e23698a7610c7295f133cdc5322737eaa983
huyenprogrammer <huyenprogrammer@gmail.com>
2023-09-26 15:12:21 +0700

    config layouts

---
+++ src/components/Layouts/DefaultLayout/index.tsx
@@ -0,0 +1,28 @@
+import classNames from "classnames";
+import { ReactNode } from "react";
+
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+import styles from "./DefaultLayout.module.scss";
+
+const cx = classNames.bind(styles);
+
+interface IProps {
+  children: ReactNode;
+}
+
+const DefaultLayout = ({ children }: IProps) => {
+  return (
+    <div className={cx("wrapper")}>
+      <Header />
+
+      <div className={cx("container")}>
+        <Sidebar />
+
+        <div className={cx("content")}>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default DefaultLayout;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,21 @@
+import React, { ReactNode } from "react";
+
+import { Home } from "@/pages";
+
 interface IRoute {
   path: string;
+  component: React.FC;
+  layout?: React.FC<{ children: ReactNode }>;
 }
 
-const publicRoutes: IRoute[] = [];
+const publicRoutes: IRoute[] = [
+  {
+    path: "/",
+    component: Home,
+  },
+];
 
 const privateRoutes: IRoute[] = [];
 
 export { privateRoutes, publicRoutes };
+
