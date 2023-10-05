031f90b71b2f60c6acad376cc99b33534dbb1506
huyenprogrammer <huyenprogrammer@gmail.com>
2023-10-05 11:13:16 +0700

    base submenu items

---
+++ src/components/Popper/Menu/Header.tsx
@@ -0,0 +1,26 @@
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames/bind";
+
+import styles from "./Menu.module.scss";
+
+const cx = classNames.bind(styles);
+
+interface IProps {
+  title: string;
+  onBack: () => void;
+}
+
+const Header = ({ title, onBack }: IProps) => {
+  return (
+    <header className={cx("header")}>
+      <button className={cx("back-btn")} onClick={onBack}>
+        <FontAwesomeIcon icon={faChevronLeft} />
+      </button>
+
+      <h4 className={cx("header-title")}> {title}</h4>
+    </header>
+  );
+};
+
+export default Header;
--- src/components/Popper/Menu/index.tsx
+++ src/components/Popper/Menu/index.tsx
@@ -1,31 +1,70 @@
 import Tippy from "@tippyjs/react/headless";
 import classNames from "classnames/bind";
+import { useState } from "react";
 
 import { PopperWrapper } from "@/components/Popper";
+import Header from "./Header";
 import styles from "./Menu.module.scss";
 import MenuItem from "./MenuItem";
-import { IItemMenu } from "./types";
+import { IMenuItem } from "./types";
 
 const cx = classNames.bind(styles);
 
 interface IProps {
   children: React.ReactElement;
-  items?: IItemMenu[];
+  items?: IMenuItem[];
+  onChange: (menuItem: IMenuItem) => void;
 }
 
-const MenuPopper = ({ children, items = [] }: IProps) => {
+const MenuPopper = ({ children, items = [], onChange }: IProps) => {
+  const [history, setHistory] = useState<IMenuItem[]>([{ data: items }]);
+
+  const current = history[history.length - 1];
+
   const renderItems = () => {
-    return items.map((item, index) => <MenuItem key={index} data={item} />);
+    if (!current.data) return;
+
+    return current.data.map((item, index) => {
+      const isParent = !!item.children;
+
+      return (
+        <MenuItem
+          key={index}
+          data={item}
+          onClick={() => {
+            if (isParent) {
+              setHistory((prevHistory) => [
+                ...prevHistory,
+                { ...item.children },
+              ]);
+            } else {
+              onChange(item);
+            }
+          }}
+        />
+      );
+    });
+  };
+
+  const handleBack = () => {
+    setHistory((prevState) => prevState.slice(0, prevState.length - 1));
   };
 
   return (
     <Tippy
+      visible
       interactive
       delay={[0, 700]}
       placement='bottom-end'
       render={(attrs) => (
         <div className={cx("menu-items")} tabIndex={-1} {...attrs}>
-          <PopperWrapper>{renderItems()}</PopperWrapper>
+          <PopperWrapper>
+            {current.title && (
+              <Header title={current.title} onBack={handleBack} />
+            )}
+
+            {renderItems()}
+          </PopperWrapper>
         </div>
       )}
     >
